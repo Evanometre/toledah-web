b5d9074fdecef4b47531501d816bb7b2905b3819
Evans <evansjohn152@gmail.com>
2026-02-28 23:57:54 +0100

    Fix Eleventy ignoring admin and content folders

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,6 +11,7 @@ module.exports = function(eleventyConfig) {
       input: ".",
       output: "_site",
       includes: "_includes"
-    }
+    },
+    templateFormats: ["html", "md", "njk"]
   };
 };
